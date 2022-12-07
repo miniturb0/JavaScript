5c8c097e5ab048f7e563cdb75c1c983650588de6
miniturb0 <adrian.foshaug@gmail.com>
2022-12-07 13:50:50 +0100

    asda

--- mathy/mathy.js
+++ mathy/mathy.js
@@ -1,3 +1,15 @@
+function stripString(str) {
+    let outStringArray = [];
+    const allowedCharacters = "0123456789+-*/^()%=<>qwertyuiopasdfghjklzxcvbnm"
+    for (let i = 0; i < str.length; i++) {
+        if (allowedCharacters.includes( str[i]) ) {
+            outStringArray.push(str[i])
+        }
+    }
+    let outString = outStringArray.join("")
+    return outString
+}
+
 function calcyp(){
     let nummer1 = document.querySelector("#num1");
     let nummer2 = document.querySelector("#num2");
@@ -30,11 +42,29 @@ function calcysqrt(){
 
     if (numall1 == ""){
         document.querySelector("#hei").innerHTML = String(numall);
+    }else if (numall == ""){
+        document.querySelector("#hei").innerHTML = String(numall1);
     }else{
         document.querySelector("#hei").innerHTML = String(numall+" and "+numall1);
     }
 
     
+}
+function calcyeval(){
+    let nummer1 = document.querySelector("#num1");
+    let nummer2 = document.querySelector("#num2");
+    let numall1 = math.evaluate(stripString( nummer2.value));
+    let numall = math.evaluate(stripString(nummer1.value));
+    console.log(nummer2.value)
+    if (nummer2.value == ""){
+        document.querySelector("#hei").innerHTML = String(numall);
+    }else if (nummer1.value == ""){
+        document.querySelector("#hei").innerHTML = String(numall1);
+    }else{
+        document.querySelector("#hei").innerHTML = String(numall+" and "+numall1);
+    }
+
+   
 }
 function resety(){
     document.querySelector("#hei").innerHTML = "";
@@ -46,4 +76,11 @@ function keepy(){
     document.querySelector("#num2").value = "";
     document.querySelector("#hei").innerHTML = "";
 }
+function calcypot(){
+    let nummer1 = document.querySelector("#num1");
+    let nummer2 = document.querySelector("#num2");
+    let numall = Number(nummer1.value) ** Number(nummer2.value);
+    document.querySelector("#hei").innerHTML = String(numall);
 
+
+}
