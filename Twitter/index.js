cf018a25f5941c1250b4fcf1c5d82bc7455795fd
miniturb0 <adrian.foshaug@gmail.com>
2023-04-15 20:13:18 +0200

    aksudha

--- Twitter/index.js
+++ Twitter/index.js
@@ -1,42 +1,37 @@
-function profilePop(e) {
-    let body = document.querySelector("body");
-    let main = document.querySelector("#profilePop");
-    let head = document.querySelector("h2");
-    head.innerHTML = e.target.innerHTML;
-    console.log("hei    ")
-    main.style.display = "block";
-
-
+profile.addEventListener("click",profilePop);
+let side = document.querySelector(".side");
+let userData = JSON.parse(localStorage.getItem("userData"));
+if (localStorage.getItem("userData")) {
+    
 }
-    let side = document.querySelector(".side");
-    let userData = JSON.parse(localStorage.getItem("userData"))
-
+// kode som legger til alle brukere under registrerte utenom den som er logget inn
     for (let i = 0; i < userData.length; i++) {
         if (userData[i].username != sessionStorage.loggedIn) {
-            let a = document.createElement("div");
-            a.href = "";
+            let mainDiv = document.createElement("div");
+            mainDiv.href = "";
             let div = document.createElement("div");
             div.innerHTML = userData[i].username;
+            div.style.cursor = "pointer";
             div.addEventListener("click",profilePop)
             let img = document.createElement("img");
-            let button = document.createElement("button");
-            if (userData[i].followers.indexOf(sessionStorage.loggedIn)==-1) {
-                button.addEventListener("click",follow);
-                button.innerHTML = "follow";
-            }else{
-                button.addEventListener("click",unfollow);
-                button.innerHTML = "unfollow";
-            }
-            button.name = userData[i].username;
+            // let button = document.createElement("button");
+            // if (userData[i].followers.indexOf(sessionStorage.loggedIn)==-1) {
+            //     button.addEventListener("click",follow);
+            //     button.innerHTML = "follow";
+            // }else{
+            //     button.addEventListener("click",unfollow);
+            //     button.innerHTML = "unfollow";
+            // }
+            // button.name = userData[i].username;
             if (userData[i].gender == "male") {
             img.src = "bilder/icon.png";
             }else{
             img.src = "bilder/iconFF.png";
             }
-            a.appendChild(img);
-            a.appendChild(div);
-            a.appendChild(button);
-            side.appendChild(a);
+            mainDiv.appendChild(img);
+            mainDiv.appendChild(div);
+            // mainDiv.appendChild(button);
+            side.appendChild(mainDiv);
         } 
     }
 
--- Twitter/twitter.js
+++ Twitter/twitter.js
@@ -8,16 +8,17 @@ function obj(un,pw,g) {
     this.gender = g;
     this.followers = [];
     this.following = [];
-    this.status = "";
+    this.status = "alive";
+    this.quacks = 0;
 }
 function signup() {
     let data = JSON.parse(localStorage.getItem("userData")) || []
     if (inp[0].value=="" || inp[1].value=="") {
-        feilm.innerHTML = "Choose both username and password"
+        feilm.innerHTML = "Choose both username and password";
         return
     }for (let i = 0; i < data.length; i++) {
         if (data[i].username == inp[0].value) {
-            feilm.innerHTML = "Username "+inp[0].value +" is already taken, choose another one"
+            feilm.innerHTML = "Username "+inp[0].value +" is already taken, choose another one";
             return
         }
     }
@@ -25,11 +26,11 @@ function signup() {
     let pw = inp[1].value;
     let g ="";
     if (inp[2].checked) {
-        g = "male"
+        g = "male";
     }else if (inp[3].checked) {
-        g = "female"
+        g = "female";
     }else{
-        feilm.innerHTML = "choose gender"
+        feilm.innerHTML = "choose gender";
         return
     }
     let newUser = new obj(un, pw, g);
@@ -37,7 +38,7 @@ function signup() {
     let existingData = JSON.parse(localStorage.getItem("userData")) || [];
     existingData.push(newUser);
     localStorage.setItem("userData", JSON.stringify(existingData));
-    feilm.innerHTML = "You have created an account "+inp[0].value
+    feilm.innerHTML = "You have created an account "+inp[0].value;
     inp[0].value = "";
     inp[1].value = "";
     inp[2].checked = false;
@@ -47,7 +48,7 @@ function login() {
     let data = JSON.parse(localStorage.getItem("userData")) || [];
     for (let i = 0; i < data.length; i++) {
         if (data[i].username == inp[0].value && data[i].password == inp[1].value) {
-            sessionStorage.loggedIn = inp[0].value
+            sessionStorage.loggedIn = inp[0].value;
             location.href = "index.html";
             return
         }
@@ -56,72 +57,138 @@ function login() {
 // utfører funksjonen nedenfor når siden har blitt lastet inn
 window.onload = function () {
     if (sessionStorage.loggedIn == undefined && window.location.href.indexOf("index.html") != -1) {
-        location.href = "login.html"
+        location.href = "login.html";
         return
     }else if (profile) {
-        profile.innerHTML = sessionStorage.loggedIn
+        profile.innerHTML = sessionStorage.loggedIn;
     }
 }
+
+
 function follow(e) {
     let username = e.target.name;
     if (username == sessionStorage.loggedIn) {
         return
     }
-    let existingData = JSON.parse(localStorage.getItem("userData"))
+    let existingData = JSON.parse(localStorage.getItem("userData"));
     for (let i = 0; i < existingData.length; i++) {
         if (existingData[i].username == username) {
-            let existingFollowers = existingData[i].followers
-            let newFollower = sessionStorage.loggedIn
-            existingFollowers.push(newFollower)
-            existingData[i].followers = existingFollowers
-            localStorage.setItem("userData",JSON.stringify(existingData))
+            let existingFollowers = existingData[i].followers;
+            let newFollower = sessionStorage.loggedIn;
+            existingFollowers.push(newFollower);
+            existingData[i].followers = existingFollowers;
+            localStorage.setItem("userData",JSON.stringify(existingData));
         }
         if (existingData[i].username == sessionStorage.loggedIn) {
-            let existingFollowing = existingData[i].following
-            existingFollowing.push(username)
-            existingData[i].following = existingFollowing
-            localStorage.setItem("userData",JSON.stringify(existingData))
+            let existingFollowing = existingData[i].following;
+            existingFollowing.push(username);
+            existingData[i].following = existingFollowing;
+            localStorage.setItem("userData",JSON.stringify(existingData));
         }
     }
-    e.target.removeEventListener("click",follow)
-    e.target.addEventListener("click",unfollow)
-    e.target.innerHTML = "unfollow"
+    e.target.removeEventListener("click",follow);
+    e.target.addEventListener("click",unfollow);
+    e.target.innerHTML = "unfollow";
 }
 function unfollow(e) {
-    let username = e.target.name;
+    let username = e.target.name;;
     if (username == sessionStorage.loggedIn) {
         return
     }
-    let existingData = JSON.parse(localStorage.getItem("userData"))
+    let existingData = JSON.parse(localStorage.getItem("userData"));
     for (let i = 0; i < existingData.length; i++) {
         if (existingData[i].username == username) {
-            let existingFollowers = existingData[i].followers
-            let newFollower = sessionStorage.loggedIn
-            let index = existingFollowers.indexOf(newFollower)
-            existingData[i].followers.splice(index,1)
-            localStorage.setItem("userData",JSON.stringify(existingData))
+            let existingFollowers = existingData[i].followers;
+            let newFollower = sessionStorage.loggedIn;
+            let index = existingFollowers.indexOf(newFollower);
+            existingData[i].followers.splice(index,1);
+            localStorage.setItem("userData",JSON.stringify(existingData));
         }
         if (existingData[i].username == sessionStorage.loggedIn) {
-            let existingFollowing = existingData[i].following
-            let index = existingFollowing.indexOf(username)
-            existingData[i].following.splice(index, 1)
-            localStorage.setItem("userData",JSON.stringify(existingData))
+            let existingFollowing = existingData[i].following;
+            let index = existingFollowing.indexOf(username);
+            existingData[i].following.splice(index, 1);
+            localStorage.setItem("userData",JSON.stringify(existingData));
         }
-}e.target.removeEventListener("click",unfollow)
-e.target.addEventListener("click",follow)
-e.target.innerHTML = "follow"
+}e.target.removeEventListener("click",unfollow);
+e.target.addEventListener("click",follow);
+e.target.innerHTML = "follow";
 }
 
 
-
+// bruke e.target.getElementsByTagName("div") for å muligens gjøre click area
+// større, da må også endre index.js kode for å lage en div rundt både img og username
 function profilePop(e) {
-    let body = document.querySelector("body");
-    let main = document.createElement("div");
-    let head = document.createElement("h2");
-    main.id = "profileP";
+    if (e.target.innerHTML == sessionStorage.loggedIn) {
+        document.querySelector("#personalProfile").style.display = "block";
+    }else{
+        document.querySelector("#personalProfile").style.display = "none";
+    }
+    let userData = JSON.parse(localStorage.getItem("userData"));
+    let main = document.querySelector("#profilePop");
+    let head = document.querySelector("#user");
+    let followers = document.querySelector("#followers");
+    let following = document.querySelector("#following");
+    let quacks = document.querySelector("#quacks");
+    let status = document.querySelector("#status");
+    let followy = document.querySelector("#follow");
+    followy.name = e.target.innerHTML
+    followy.addEventListener("click",follow)
+    for (let i = 0; i < userData.length; i++) {
+        if (userData[i].username == e.target.innerHTML) {
+            followers.innerHTML = userData[i].followers.length;
+            status.innerHTML = userData[i].status;
+            following.innerHTML = userData[i].following.length;
+            quacks.innerHTML = userData[i].quacks;
+        }
+    }
     head.innerHTML = e.target.innerHTML;
-    main.appendChild(head)
-    body.appendChild(main)
+    main.style.display = "block";
+}
+function setStatus() {
+    let statusInp = document.querySelector("#statusInp");
+    let status = document.querySelector("#status");
+    let newStatus = statusInp.value;
+    let head = document.querySelector("#user");
+    status.innerHTML = newStatus;
+    let userData = JSON.parse(localStorage.getItem("userData"));
+    for (let i = 0; i < userData.length; i++) {
+        if (userData[i].username == head.innerHTML) {
+            userData[i].status = newStatus;
+        }
+    }
+    statusInp.value = "";
+    localStorage.setItem("userData",JSON.stringify(userData))
+}
 
-
+// endrer fra antall followers og following til navnene istedet og bytter om annen hvergang
+// ved hjelp av nN
+let nN = 0;
+function names() {
+    let userData = JSON.parse(localStorage.getItem("userData"));
+    let followers = document.querySelector("#followers");
+    let following = document.querySelector("#following");
+    let head = document.querySelector("#user");
+    if (nN == 0) {
+        for (let i = 0; i < userData.length; i++) {
+            if (userData[i].username == head.innerHTML) {
+                let string1 = userData[i].followers.toString();
+                followers.innerHTML = string1;
+                let string2 = userData[i].following.toString();
+                following.innerHTML = string2;
+            }
+        }
+        nN=1
+    }else{
+        for (let i = 0; i < userData.length; i++) {
+            if (userData[i].username == head.innerHTML) {
+                let string1 = userData[i].followers.length;
+                followers.innerHTML = string1;
+                let string2 = userData[i].following.length;
+                following.innerHTML = string2;
+            }
+        }
+        nN=0
+    }
+    
 }
