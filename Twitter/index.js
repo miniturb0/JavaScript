0ee39b0978a1eb15d7b3b77dcb94834e200d1a57
miniturb0 <adrian.foshaug@gmail.com>
2023-04-17 22:25:18 +0200

    asdad

--- Twitter/index.js
+++ Twitter/index.js
@@ -6,33 +6,23 @@ if (localStorage.getItem("userData")==null) {
     sessionStorage.clear()
 }
 // kode som legger til alle brukere under registrerte utenom den som er logget inn
-    for (let i = 0; i < userData.length; i++) {
-        if (userData[i].username != sessionStorage.loggedIn) {
-            let mainDiv = document.createElement("div");
-            mainDiv.href = "";
-            let div = document.createElement("div");
-            div.innerHTML = userData[i].username;
-            div.style.cursor = "pointer";
-            div.addEventListener("click",profilePop)
-            let img = document.createElement("img");
-            // let button = document.createElement("button");
-            // if (userData[i].followers.indexOf(sessionStorage.loggedIn)==-1) {
-            //     button.addEventListener("click",follow);
-            //     button.innerHTML = "follow";
-            // }else{
-            //     button.addEventListener("click",unfollow);
-            //     button.innerHTML = "unfollow";
-            // }
-            // button.name = userData[i].username;
-            img.src = "bilder/iconFF.png";
-            if (userData[i].gender == "male") {
-                img.src = "bilder/icon.png";
-            }
-            mainDiv.appendChild(img);
-            mainDiv.appendChild(div);
-            // mainDiv.appendChild(button);
-            side.appendChild(mainDiv);
-        } 
-    }
+    // for (let i = 0; i < userData.length; i++) {
+    //     if (userData[i].username != sessionStorage.loggedIn) {
+    //         let mainDiv = document.createElement("div");
+    //         mainDiv.href = "";
+    //         let div = document.createElement("div");
+    //         div.innerHTML = userData[i].username;
+    //         div.style.cursor = "pointer";
+    //         div.addEventListener("click",profilePop)
+    //         let img = document.createElement("img");
+    //         img.src = "bilder/iconFF.png";
+    //         if (userData[i].gender == "male") {
+    //             img.src = "bilder/icon.png";
+    //         }
+    //         mainDiv.appendChild(img);
+    //         mainDiv.appendChild(div);
+    //         side.appendChild(mainDiv);
+    //     } 
+    // }
 
 
